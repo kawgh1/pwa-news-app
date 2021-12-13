d93bad5eb603ac906b4eed699533ca3f66a90fcb
kawgh1 <krseven@gmail.com>
2021-12-13 13:29:49 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import "./news-article.js";
+import { topHeadlinesUrl } from "./newsApi.js";
+
+// wait for page to load before calling getNews Api and registering service worker
+window.addEventListener("load", () => {
+    getNews();
+    registerSW();
+});
+
+async function getNews() {
+    const res = await fetch(topHeadlinesUrl);
+    const json = await res.json();
+
+    const main = document.querySelector("main");
+
+    json.articles.forEach((article) => {
+        const el = document.createElement("news-article");
+        el.article = article;
+        main.appendChild(el);
+    });
+}
+
+async function registerSW() {
+    if ("serviceWorker" in navigator) {
+        try {
+            await navigator.serviceWorker.register("./sw.js");
+        } catch (e) {
+            console.log(`SW registration failed`);
+        }
+    }
+}
---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = "0027725ae0b14e118c713554cee36843";
+const topHeadlinesUrl =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + apiKey;
+
+export { apiKey, topHeadlinesUrl };
